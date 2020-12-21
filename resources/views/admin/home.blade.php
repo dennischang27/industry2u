8e665811167a83c0b4ae8954dd78d437794494c7
JIA SONG <jiasong23@gmail.com>
2020-12-21 23:11:52 +0800

    Home view for admin

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,29 @@
+@extends('admin.layouts.app')
+@section('title')
+    <title>{{ config('app.name', 'Industry2u') .  __(' Admin Portal') }}</title>
+@endsection
+
+@section('pagetitle')
+
+@endsection
+
+@section('content')
+    <!-- Content Row -->
+    <div class="row">
+
+        <!-- Earnings (Monthly) Card Example -->
+        <div class="col-xl-12 col-md-12 mb-12">
+            <!-- Illustrations -->
+            <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">Introduction</h6>
+                </div>
+                <div class="card-body">
+
+                    <p>To be added</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Content Row -->
+@endsection
