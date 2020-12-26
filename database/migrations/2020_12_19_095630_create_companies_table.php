a98c54e33485e4397ebcb9d4b503fe750eb1b28d
JIA SONG <jiasong23@gmail.com>
2020-12-26 16:16:05 +0800

    Added company budget ranges foreign

--- database/migrations/2020_12_19_095630_create_companies_table.php
+++ database/migrations/2020_12_19_095630_create_companies_table.php
@@ -24,6 +24,8 @@ class CreateCompaniesTable extends Migration
             $table->unsignedBigInteger('country_id')->nullable();
             $table->foreign('industry_id')->references('id')->on('industries');
             $table->unsignedBigInteger('industry_id')->nullable();
+            $table->foreign('company_budget_range_id')->references('id')->on('company_budget_ranges');
+            $table->unsignedBigInteger('company_budget_range_id')->nullable();
             $table->string('reg_no', 150);
             $table->string('web_site', 150)->nullable();
             $table->string('phone', 150);
