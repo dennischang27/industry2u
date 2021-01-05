50ab5d9b0ec5d1ede74494bb14b7321a74be387e
JIA SONG <jiasong23@gmail.com>
2021-01-05 12:35:41 +0800

    Updated for sidebar active check

--- resources/views/seller/layouts/sidebar.blade.php
+++ resources/views/seller/layouts/sidebar.blade.php
@@ -19,13 +19,13 @@
                             <li class="{{  request()->routeIs('seller.products.create') ? 'active' : '' }}"><a href="{{ route('seller.products.create') }}">Add New Product</a></li>
                         </ul>
                     </li>
-                    <li class="{{  request()->routeIs('seller.account') ? 'active' : '' }} {{  request()->routeIs('seller.company.profile') ? 'active' : '' }}">
+                    <li class="{{  request()->routeIs('seller.account') ? 'active' : '' }} {{  request()->routeIs('seller.company.*') ? 'active' : '' }}">
                         <a href="javascript:void(0)" aria-expanded="true">
                             <i class="ti-map-alt"></i>
                             <span>Company</span>
                         </a>
                         <ul class="collapse">
-                            <li class="{{  request()->routeIs('seller.company.profile') ? 'active' : '' }}"><a href="{{ route('seller.company.profile') }}">Company Profile</a></li>
+                            <li class="{{  request()->routeIs('seller.company.profile') ? 'active' : '' }} {{  request()->routeIs('seller.company.profile.*') ? 'active' : '' }}"><a href="{{ route('seller.company.profile') }}">Company Profile</a></li>
                             <li class="{{  request()->routeIs('seller.account') ? 'active' : '' }}"><a href="{{ route('seller.account') }}">Account</a></li>
                         </ul>
                     </li>
