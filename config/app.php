1e3c096f2f7aa94a80fff0f8c7029bdf7432094a
JIA SONG <jiasong23@gmail.com>
2021-02-19 14:11:41 +0800

    bug fixed

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         Illuminate\View\ViewServiceProvider::class,
         Spatie\Permission\PermissionServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
+        Spatie\CookieConsent\CookieConsentServiceProvider::class,
 
         /*
          * Package Service Providers...
