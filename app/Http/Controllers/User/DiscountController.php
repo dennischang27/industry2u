ea270cff0c8c70dbeb7651f4c4e72a4271302da4
Huey Wen <hueywen8018@gmail.com>
2021-03-11 18:25:51 +0800

    [ management - centre | price-management ] - removed unused code and files

--- app/Http/Controllers/User/DiscountController.php
+++ app/Http/Controllers/User/DiscountController.php
@@ -23,20 +23,6 @@ class DiscountController extends Controller{
         return view('user.discount.master', compact('discount'));
     } 
 
-    public function manager(){
-        $user = Auth::getUser()->id;
-        $discount = Discount::where('user_id', $user)->first();
-
-        return view('user.discount.manager', compact('discount'));
-    } 
-
-    public function sales(){
-        $user = Auth::getUser()->id;
-        $discount = Discount::where('user_id', $user)->first();
-
-        return view('user.discount.sales', compact('discount'));
-    } 
-
     public function masterCreate(){
 
         // $discount = new Discount();
@@ -71,68 +57,5 @@ class DiscountController extends Controller{
 
     }
 
-    public function managerCreate(){
-
-        // $discount = new Discount();
-        $isUserExist = Discount::where('user_id', '=', Auth::user()->id)->count();
-        $user = Auth::getUser()->id;
-        $discount = Discount::where('user_id', $user)->first();
-
-        if($isUserExist == 0 ) {
-            $discount = new Discount();
-    
-            $discount->manager_tier1 = request('manager_tier1');
-            $discount->manager_tier2 = request('manager_tier2');
-            $discount->manager_tier3 = request('manager_tier3');
-            $discount->user_id = $user;
-    
-            $discount->save();
-
-        } else {
-            $discount = Discount::where('user_id', $user)->first();
-
-            $discount->manager_tier1 = request('manager_tier1');
-            $discount->manager_tier2 = request('manager_tier2');
-            $discount->manager_tier3 = request('manager_tier3'); 
-
-            $discount->save();
-        }
-
-        return view('user.discount.index', compact('user', 'discount'));
-
-    }
-
-    public function salesCreate(){
-
-        // $discount = new Discount();
-        $isUserExist = Discount::where('user_id', '=', Auth::user()->id)->count();
-        $user = Auth::getUser()->id;
-        $discount = Discount::where('user_id', $user)->first();
-
-        if($isUserExist == 0 ) {
-            $discount = new Discount();
-    
-            $discount->sales_tier1 = request('sales_tier1');
-            $discount->sales_tier2 = request('sales_tier2');
-            $discount->sales_tier3 = request('sales_tier3');
-            $discount->user_id = $user;
-    
-            $discount->save();
-
-        } else {
-
-
-            $discount = Discount::where('user_id', $user)->first();
-
-            $discount->sales_tier1 = request('sales_tier1');
-            $discount->sales_tier2 = request('sales_tier2');
-            $discount->sales_tier3 = request('sales_tier3'); 
-
-            $discount->save();
-        }
-
-        return view('user.discount.index', compact('user', 'discount'));
-
-    }
 
 }
