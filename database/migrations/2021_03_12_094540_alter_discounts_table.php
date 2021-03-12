72156b590dff5fe1e8747e37061332215860a043
Huey Wen <hueywen8018@gmail.com>
2021-03-12 11:36:38 +0800

    [ Management Center | Price Management ] - added alter discount table

---
+++ database/migrations/2021_03_12_094540_alter_discounts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterDiscountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('discounts', function (Blueprint $table) {
+            $table->dropColumn(['manager_tier1', 'manager_tier2', 'manager_tier3', 'sales_tier1', 'sales_tier2', 'sales_tier3'] );
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        $table->integer('manager_tier1')->nullable();
+        $table->integer('manager_tier2')->nullable();
+        $table->integer('manager_tier3')->nullable();
+        $table->integer('sales_tier1')->nullable();
+        $table->integer('sales_tier2')->nullable();
+        $table->integer('sales_tier3')->nullable();
+    }
+}
