588979db02721b00cb861427eadb49f748038a57
Dennis <dennis@industry2u.asia>
2021-03-12 20:49:07 +0800

    Added Bank info

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -19,6 +19,12 @@ use Carbon\Carbon;
 use Hash;
 use Image;
 use App\Rules\MatchOldPassword;
+use Spatie\Permission\Models\Role;
+use Redirect;
+use App\Models\Designations;
+use Session;
+use Mail;
+use DB;
 
 class UserController extends Controller
 {
@@ -192,6 +198,83 @@ class UserController extends Controller
         return redirect()->route('user.changepassword')
             ->with('success','User password has been updated.');
     }
+	
+	public function bankinfo() {
+		$user = parent::getUser();
+        $company = Company::where('user_id', $user->id)->first();
+        $sst_file_path = '';
+        $sst_file_name = '';
 
+        if($company) {
+            foreach($company->companyDocs as $document) {
+                $myssmDocuments[$document->doc_type_id] = $document;
+                if($document['doc_type_id'] == 4){
+                    $sst_file_path = $document['file_path'];
+                    $sst_file_name = $document->doc_type->name;
+                }
+            }
+        }
 
+        return view('user.bankinfo',compact('company','sst_file_path','sst_file_name'));
+    }
+
+    public function bankinfoadd() {
+        $user = parent::getUser();
+        $company = Company::where('user_id', $user->id)->first();
+        $bank=Bank::all();
+        $doc_type_sst = DocType::where('type','SST')->get();
+        return view('user.bankinfo_add',compact('company','bank','doc_type_sst'));
+    }
+
+    public function bankinfoaddpost(Company $company, Request $request) {
+        $this->validate(request(), [
+            'bank_id' => 'required',
+			'bank_account' => 'required',
+        ]);
+
+        $input = $request->all();
+        $input["is_seller"] = 1;
+        $company->update($input);
+
+        if($sstdocFiles = $request->file('sstfile')) {
+            foreach($sstdocFiles as $key => $doc) {
+                if($doc) {
+                    $doc_type = DocType::find($key);
+
+                    if($doc_type) {
+                        $path = $doc->storeAs('companies/' . $company->id, $doc_type->name . "." . $doc->getClientOriginalExtension(), 'public');
+
+                        $document = $company->companyDocs->where('doc_type_id', $key)->first();
+                        if($document) {
+                            $document->update([
+                                'file_path' =>  $path,
+                            ]);
+                        } else {
+                            $company->CompanyDocs()->create([
+                                'file_path' => $path,
+                                'doc_type_id' => $doc_type->id,
+                            ]);
+                        }
+
+                    }
+                }
+            }
+        }
+
+        $user = Auth::user();
+        $user->is_seller = 1;
+        $user->save();
+
+        $user = parent::getUser();
+        $company = Company::where('user_id', $user->id)->first();
+        return redirect()->route("user.bankinfo");
+    }
+	
+	public function bankinfoedit() {
+        $user = parent::getUser();
+        $company = Company::where('user_id', $user->id)->first();
+        $bank=Bank::all();
+        $doc_type_sst = DocType::where('type','SST')->get();
+        return view('user.bankinfo_edit',compact('company','bank','doc_type_sst'));
+    }
 }
