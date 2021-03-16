39cefe2326b3f2f36818c2deccecf867dfd121ea
Dennis <dennis@industry2u.asia>
2021-03-16 15:17:16 +0800

    Added designation_id column

---
+++ database/migrations/2021_03_16_111814_alter_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->bigInteger('designation_id')->nullable()->unsigned()->after('status');
+            $table->foreign('designation_id')->nullable()->references('id')->on('roles');
+     });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign('designation_id');
+            $table->dropColumn('designation_id');
+        });
+    }
+}
