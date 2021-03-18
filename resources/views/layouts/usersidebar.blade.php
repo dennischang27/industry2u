34ce73b8a38cd58ea713277cd0de80a0442f6f8d
Dennis <dennis@industry2u.asia>
2021-03-18 16:21:27 +0800

    Added user side menu handling

--- resources/views/layouts/usersidebar.blade.php
+++ resources/views/layouts/usersidebar.blade.php
@@ -1,17 +1,27 @@
+<style>
+.nav-link[data-toggle].collapsed:after {
+    content: " ▾";
+}
+.nav-link[data-toggle]:not(.collapsed):after {
+    content: " ▴";
+}
+</style>
 <div class="dashboard_menu">
     <ul class="nav nav-tabs flex-column" role="tablist">
         <li class="nav-item">
             <a class="nav-link {{  request()->routeIs('user.account') ? 'active' : '' }}" href="{{ route("user.account") }}"><i class="ti-layout-grid2"></i>Account</a>
         </li>
-        @if (!auth('web')->user()->is_buyer)
-        <li class="nav-item">
-            <a class="nav-link  {{  request()->routeIs('addcompany') ? 'active' : '' }}" href="{{ route("addcompany") }}"><i class="ti-map-alt"></i>Add Company</a>
-        </li>
-        @else
-        <li class="nav-item">
-            <a class="nav-link  {{  request()->routeIs('user.company') ? 'active' : '' }} {{  request()->routeIs('user.company.*') ? 'active' : '' }}" href="{{ route("user.company") }}"><i class="ti-map-alt"></i>Company Profile</a>
-        </li>
-        @endif
+        @role('Admin')
+            @if (!auth('web')->user()->is_buyer)
+            <li class="nav-item">
+                <a class="nav-link  {{  request()->routeIs('addcompany') ? 'active' : '' }}" href="{{ route("addcompany") }}"><i class="ti-map-alt"></i>Add Company</a>
+            </li>
+            @else
+            <li class="nav-item">
+                <a class="nav-link  {{  request()->routeIs('user.company') ? 'active' : '' }} {{  request()->routeIs('user.company.*') ? 'active' : '' }}" href="{{ route("user.company") }}"><i class="ti-map-alt"></i>Company Profile</a>
+            </li>
+            @endif
+        @endrole
         <li class="nav-item">
             <a class="nav-link  {{  request()->routeIs('user.profile') ? 'active' : '' }}" href="{{ route("user.profile") }}"><i class="ti-id-badge"></i>Edit Profile</a>
         </li>
