1bbb58bac1c694469227138dc85b81b34d631758
Huey Wen <hueywen8018@gmail.com>
2021-03-29 10:44:19 +0800

    [ Sales Center | invite customer ] - updated mail template

--- resources/views/user/sales/customers/mailnewcustomerinvite.blade.php
+++ resources/views/user/sales/customers/mailnewcustomerinvite.blade.php
@@ -5,14 +5,31 @@
 </head>
 <body>
     <p>Hello, Good Day!</p>
-    <p>I am delighted to invite you on behalf of {{ $company }} to become their customer in Industry2u - Industry ecommerce system</p>
-       
-    <p>Click Here to join today.</p>
-    <p>Register <a href="{{ route('register') }}?code={{ $invitation_code }}">Here</a></p>
 
     <br>
-    <p>On behalf of</p>
+
+    <p>I am delighted to invite you on behalf of {{ $company }} to register as Purchaser and become their customer in Industry2u - Industry ecommerce system.</p>
+
+    <br>
+
+    <p>
+        Steps to follow to register an account and become customer of {{ $company }}:
+    </p>
+        <ol>
+            <li>Register an account in Industry2u - Industry ecommerce system</li>
+            <li>Submit the company information included the SSM Form 9 to become a Purchaser</li>
+            <li>Join to become a customer of {{ $company }}</li>
+        </ol>
+
+    <p> Click  <a href="{{ route('register') }}?code={{ $invitation_code }}"> Here</a> to register today. </p>
+
+    <br>
+    <p>Thank You!</p>
+    <br><br>
+
+    <p>On behalf of</p> 
     <p>{{ $company }}</p>
+
 
 
 </body>
